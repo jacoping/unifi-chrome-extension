d7a76ee96ff76a44f216371d4a565b33c71815ee
Jacopo <jacoping@gmail.com>
2020-04-03 16:55:03 +0200

    add sanitize-html library

--- content.js
+++ content.js
@@ -1,4 +1,14 @@
+
+
 console.log("UNIFI-CHROME: content.js loaded");
+
+const settings = {
+  selector_name: '#cmpro-pagecontent'
+}
+
+// hello world sanitize html
+var html = "<strong>hello world</strong>";
+console.log(sanitizeHtml(html));
 
 var original_content = document.querySelector('body');
 var status = "dirty";
@@ -6,24 +16,34 @@ var status = "dirty";
 var wash = function(body) {
   let washed = body;
   console.log("washing...");
-  let container = washed.querySelector('#cmpro-page-container');
+  let container = washed.querySelector(settings.selector_name);
+
   let elements = container.getElementsByTagName('*');
   l = elements.length;
   for (i = 0; i < l; i++) {
     elements[i].removeAttribute('style');
   }
+
   return washed;
 };
 
+var wash2 = function(body) {
+  let washed = body;
+  console.log("washing...");
+  let container = washed.querySelector(settings.selector_name);
+  container.innerHTML = sanitizeHtml(container.innerHTML);
+  return washed;
+}
+
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(request.command);
+    console.log("UNIFI-CHROME: received command: " + request.command);
 
     if (request.command === "clean") {
       // ADD CODE HERE
       let body = document.querySelector('body');
       let clonedBody = body.cloneNode(true);
-      clonedBodyWashed = wash(clonedBody);
+      clonedBodyWashed = wash2(clonedBody);
       body.replaceWith(clonedBodyWashed);
 
       status = "whashed";
