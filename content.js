41f528ad6926a591605c6a5829b2ecd35d083987
Jacopo <jacoping@gmail.com>
2020-04-03 17:08:30 +0200

    add sanitize-html options

--- content.js
+++ content.js
@@ -6,6 +6,27 @@ const settings = {
   selector_name: '#cmpro-pagecontent'
 }
 
+const sanitize_html_options = {
+  allowedTags: [ 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
+  'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
+  'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'iframe' ],
+disallowedTagsMode: 'discard',
+allowedAttributes: {
+  a: [ 'href', 'name', 'target' ],
+  // We don't currently allow img itself by default, but this
+  // would make sense if we did. You could add srcset here,
+  // and if you do the URL is checked for safety
+  img: [ 'src' ]
+},
+// Lots of these won't come up by default because we don't allow them
+selfClosing: [ 'img', 'br', 'hr', 'area', 'base', 'basefont', 'input', 'link', 'meta' ],
+// URL schemes we permit
+allowedSchemes: [ 'http', 'https', 'ftp', 'mailto' ],
+allowedSchemesByTag: {},
+allowedSchemesAppliedToAttributes: [ 'href', 'src', 'cite' ],
+allowProtocolRelative: true
+}
+
 // hello world sanitize html
 var html = "<strong>hello world</strong>";
 console.log(sanitizeHtml(html));
@@ -13,25 +34,11 @@ console.log(sanitizeHtml(html));
 var original_content = document.querySelector('body');
 var status = "dirty";
 
-var wash = function(body) {
+var sanitize = function(body) {
   let washed = body;
   console.log("washing...");
   let container = washed.querySelector(settings.selector_name);
-
-  let elements = container.getElementsByTagName('*');
-  l = elements.length;
-  for (i = 0; i < l; i++) {
-    elements[i].removeAttribute('style');
-  }
-
-  return washed;
-};
-
-var wash2 = function(body) {
-  let washed = body;
-  console.log("washing...");
-  let container = washed.querySelector(settings.selector_name);
-  container.innerHTML = sanitizeHtml(container.innerHTML);
+  container.innerHTML = sanitizeHtml(container.innerHTML, sanitize_html_options);
   return washed;
 }
 
@@ -43,7 +50,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       // ADD CODE HERE
       let body = document.querySelector('body');
       let clonedBody = body.cloneNode(true);
-      clonedBodyWashed = wash2(clonedBody);
+      clonedBodyWashed = sanitize(clonedBody);
       body.replaceWith(clonedBodyWashed);
 
       status = "whashed";
