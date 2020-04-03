b702cd25338cf615d7a338b1e600e5a4feb5ad15
Jacopo <jacoping@gmail.com>
2020-04-03 10:55:36 +0200

    make auto clean on first click

--- popup.js
+++ popup.js
@@ -1,24 +1,36 @@
 console.log("UNIFI-CHROME: popup.js loaded");
 
-document.addEventListener('DOMContentLoaded', function() {
-  var cleanButton = document.querySelector('#clean-button');
-  cleanButton.addEventListener('click', function() {
+
+let clean = function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {command: "clean"}, function(response) {
                 console.log(response.result);
+                document.querySelector("#status").innerHTML = "pulito";
             });
         });
-  }, false);
+  }
 
-  var restoreButton = document.querySelector('#restore-button');
-  restoreButton.addEventListener('click', function() {
+  let unclean = function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {command: "restore"}, function(response) {
                 console.log(response.result);
+                document.querySelector("#status").innerHTML = "originale";
             });
         });
-  }, false);
+  }
 
+document.addEventListener('DOMContentLoaded', function() {
+  var cleanButton = document.querySelector('#clean-button');
+  cleanButton.addEventListener('click', clean, false);
+
+  var restoreButton = document.querySelector('#restore-button');
+  restoreButton.addEventListener('click', unclean, false);
+
+}, false);
+
+clean();
+
+/*
   var reloadButton = document.querySelector('#reload-button');
   reloadButton.addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -27,7 +39,11 @@ document.addEventListener('DOMContentLoaded', function() {
   }, false);
 
 
-}, false);
+
+*/
+
+
+// automatic clean on first click
 
 
 /*
